48035d50f8c62f6373e2d160a4586589a7a94d5b
Arthur Stephan <arthurstephan13@gmail.com>
2020-10-04 20:27:50 -0300

    Connecting and sending data to InfluxDB

--- leitor_temperatura_umidade/leitor_temperatura_umidade.ino
+++ leitor_temperatura_umidade/leitor_temperatura_umidade.ino
@@ -1,34 +1,75 @@
-#include <ESP8266WiFi.h>
+// Biblioteca de wifi
+#include <ESP8266WiFiMulti.h>
+// Biblioteca de leitura de temperatura e umidade
 #include "dht.h"
+// Biblioteca InfluxDB
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
 
-const char* ssid = "Nome da sua rede";
-const char* password = "Senha da rede";
+// Configuração Wi-fi
+#define WIFI_SSID "Nome da Rede"
+#define WIFI_PASSWORD "Senha da rede"
 
+
+// Configuração InfluxDB
+#define INFLUXDB_URL "http://Ip da máquina onde o influx está instalado:9999"
+// Token de autenticação do InfluxDB (Use: InfluxDB UI -> Data -> Tokens -> <select token>)
+#define INFLUXDB_TOKEN "Token de autentificação"
+// Id da organização (Use: InfluxDB UI -> User -> About -> Common Ids )
+#define INFLUXDB_ORG "Id Organização"
+// Id do bucket do InfluxDB(Use: InfluxDB UI ->  Data -> Buckets)
+#define INFLUXDB_BUCKET "Bucket"
+
+// Definição Time Zone
+#define TZ_INFO "BRST+3BRDT+2,M10.3.0,M2.3.0"
+
+// Instancia do cliente influx
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+
+// Instância do Wifi
+ESP8266WiFiMulti wifiMulti;
+
+// Definição do pino para leitura
 const int pinoDHT11 = D7;
- 
+
+// Instância DHT 
 dht DHT;
- 
-void setup()
+
+// Definição do nome da measurement
+Point sensor("Sensor 1");
+
+
+void setup() 
   {
 
-    Serial.begin(115200);
-  
-    WiFi.begin(ssid, password);
-  
-    while (WiFi.status() != WL_CONNECTED)
-      {
-        delay(500);
-      }
+    // Conectando Wifi
+    WiFi.mode(WIFI_STA);
+    wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+    // Sincronização Time Zone
+    timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
   }
- 
-void loop()
+
+void loop() 
   {
+
+    // Limpeza dos Fields
+    sensor.clearFields();
+
+    // Leitura dos dados de Temperatura e Umidade
     DHT.read11(pinoDHT11);
-    Serial.print("Umidade: ");
-    Serial.print(DHT.humidity);
-    Serial.print("%");
-    Serial.print(" / Temperatura: ");
-    Serial.print(DHT.temperature, 0);
-    Serial.println("*C");
+
+    // Adicionando os Fields de Umidade de Temperatura
+    sensor.addField("Umidade",DHT.humidity);
+    sensor.addField("Temperatura", DHT.temperature);
+
+    // Transforma os dado para o padrão Line Protocol
+    sensor.toLineProtocol();
+
+    // Insere os dados no InfluxDB
+    client.writePoint(sensor);    
+
+    // Delay de 10 segundos
     delay(10000);
   }
